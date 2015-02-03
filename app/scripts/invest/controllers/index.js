45a02ad2b51ffe259119b4a2530b334f9201e872
fuwenbin <187210346@qq.com>
2015-02-03 20:14:16 +0800

    整合代码

--- app/scripts/invest/controllers/index.js
+++ app/scripts/invest/controllers/index.js
@@ -8,14 +8,14 @@
     InvestIndexController.$inject = ['$rootScope', '$scope', '$location'];
 
     function InvestIndexController($rootScope, $scope, $location) {
-        $scope.childState = '';         
+        $scope.childState = '';
         $scope.accountType = {
             key: 'demo',      //'demo' or 'real'
             value: '模拟',    // '模拟' or '真实'
             visible: false    // 切换真实模拟账户按钮的可见性
         };
         $scope.switchAccount = switchAccount;
-
+        $scope.touchCode = $location.search().touchCode;
         $scope.$on('$viewContentLoaded', function () {
             var url = $location.path();
             $scope.childState = url.split('/')[2];
@@ -26,12 +26,12 @@
 
             if ($scope.childState === 'statistics' || $scope.childState === 'history') {
                 $scope.accountType.visible = true;
-            } 
+            }
         });
 
         function switchAccount() {
             $scope.$broadcast('showLoadingImg');
-            
+
             if ($scope.accountType.key === 'real') {
                 $scope.accountType.key = 'demo';
                 $scope.accountType.value = '模拟';
