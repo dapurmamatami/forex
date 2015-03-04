aff98e860d83e330dafd36446dba24aa36871fb7
wangxiaolong <wangxiaolong@tigerbrokers.com>
2015-03-04 19:24:28 +0800

    ADD: 资金管理 入金到户样式完成

---
+++ app/scripts/money/controllers/deposit.js
@@ -0,0 +1,14 @@
+;
+(function () {
+    'use strict';
+
+    angular
+        .module('tigerwitPersonalApp')
+        .controller('MoneyDepositController', MoneyDepositController);
+
+    MoneyDepositController.$inject = ['$scope', 'money'];
+
+    function MoneyDepositController($scope, money) {
+        
+    }
+})();
---
+++ app/scripts/money/controllers/withdraw.js
@@ -0,0 +1,14 @@
+;
+(function () {
+    'use strict';
+
+    angular
+        .module('tigerwitPersonalApp')
+        .controller('MoneyWithdrawController', MoneyWithdrawController);
+
+    MoneyWithdrawController.$inject = ['$scope', 'money'];
+
+    function MoneyWithdrawController($scope, money) {
+        
+    }
+})();
