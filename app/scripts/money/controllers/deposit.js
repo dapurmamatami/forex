cb7c2ac6f871e816294fc39ac34004aa6eab0e5d
wangxiaolong <wangxiaolong@tigerbrokers.com>
2015-03-06 15:17:47 +0800

    MOD: 入金功能完成

--- app/scripts/money/controllers/deposit.js
+++ app/scripts/money/controllers/deposit.js
@@ -22,8 +22,6 @@
             }
         };
         $scope.deposit = deposit;
-        var date,
-            w;
 
         money.getDepositAmnt().then(function (data) {
             $scope.deposit.minAmount = data.limit;
@@ -31,32 +29,70 @@
 
         money.getFXRate().then(function (data) {
             $scope.FXRate.value = data.parity;
-            date = new Date(data.update_date * 1000);
+            var date = new Date(data.update_date * 1000);
             $scope.FXRate.date.year = date.getFullYear();
             $scope.FXRate.date.month = date.getMonth() + 1;
             $scope.FXRate.date.day = date.getDate();
         });
 
-        function deposit() {
+        function deposit(amount) {
+            var newAmount,
+                w;
+
+            // 判断是否注册真实账户    
+            if ($scope.personal.verified) {
+                openTipModal('sm');
+                return;
+            }
+
+            // 是选择金额还是手输金额
+            if (typeof amount === 'undefined') {
+                newAmount = Number($scope.deposit.amount).toFixed(2);
+            } else {
+                newAmount = Number(amount).toFixed(2);
+            }
+            
             w = $window.open('#/static/waiting');
 
-            money.deposit(Number($scope.deposit.amount).toFixed(2)).then(function (data) {
+            money.deposit(newAmount).then(function (data) {
                 if (!data.is_succ) return;
                 openDepositModal();
                 w.location = data.url; 
             });
         }
 
+        // 入金后的窗口，有支付遇到问题和支付成功俩选项
         function openDepositModal() {
             $modal.open({
                 templateUrl: 'views/money/deposit_modal.html',
+                controller: function ($scope, $modalInstance) {
+                    $scope.closeModal = closeModal;
+                    $scope.openDialog = openDialog;
+
+                    function closeModal() {
+                        $modalInstance.dismiss();
+                    }
+
+                    function openDialog() {
+                        OpenChat();
+                        closeModal();
+                    }
+                }
+            });
+        }
+
+        // 未注册真实账户时入金打开该窗口
+        function openTipModal(size) {
+            $modal.open({
+                templateUrl: 'views/money/tip_modal.html',
                 controller: function ($scope, $modalInstance) {
                     $scope.closeModal = closeModal;
 
                     function closeModal() {
                         $modalInstance.dismiss();
                     }
-                }
+                },
+                size: size
             });
         }
     }
