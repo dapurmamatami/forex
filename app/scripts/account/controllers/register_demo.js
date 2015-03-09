d8b9b21199282adebf90e1dfc186ac5ad5fef8c4
wangxiaolong <wangxiaolong@tigerbrokers.com>
2015-03-09 20:30:09 +0800

    ADD: register demo 样式完成

---
+++ app/scripts/account/controllers/register_demo.js
@@ -0,0 +1,14 @@
+;
+(function () {
+    'use strict';
+
+    angular
+        .module('tigerwitPersonalApp')
+        .controller('AccountRegisterDemoController', AccountRegisterDemoController);
+
+    AccountRegisterDemoController.$inject = ['$scope', 'account'];
+
+    function AccountRegisterDemoController($scope, account) {
+
+    }
+})();
