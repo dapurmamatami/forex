252354e064905cadacbfe20c6cb02544a0375ec6
wangxiaolong <wangxiaolong@tigerbrokers.com>
2015-03-12 20:23:58 +0800

    MOD: 上传头像功能完成---base64 大小判断未实现

--- app/scripts/setting/directives/crop_image.js
+++ app/scripts/setting/directives/crop_image.js
@@ -6,32 +6,46 @@
     .module('tigerwitPersonalApp')
     .directive('twCropImage', twCropImage);
 
+
+    // 上传头像功能使用该指令，上传裁剪生成的 base64
     function twCropImage() {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-                
-                scope.$watch('personal', function (newVal, oldVal) {
-                    
-                    if (!newVal) {
-                        return;
-                    }
-                    var image = $(element).find('.upload_portrait__image');
+                scope.imgDataUrl = '';
+                scope.support = true;   
+                var image = $(element).find('#image');
+                var input = $(element).find('#input');
+
+                if (Object.prototype.toString.call(window.URL) !== '[object Function]') {
+                    scope.support = false;
+                    return;
+                }
+
+                scope.$watch('personal', function (newVal) {
+                    if (!newVal) return;
+
                     image.cropper({
                         aspectRatio: 1,
                         data: {
-                            width: 150,
-                            height: 150
                         },
-                        resizable: false,
+                        minWidth: 150,
+                        minHeight: 150,
+                        resizable: true,
                         zoomable: false,
                         dragCrop: false,
-                        preview: '.upload_portrait__preview',
+                        preview: '#imgPreview',
                         done: function(data) {
-                            console.info(image.cropper('getData', true));
+                            scope.imgDataUrl = image.cropper('getDataURL');
                         }
                     });
                 }, true);
+
+                
+                input.bind('change', function () {
+                    image.cropper('replace', window.URL.createObjectURL(this.files[0]));
+                });
+                
             }
         };
     }
