af028d54bd79776c7e0e4da4697eea5bf5f3c7ca
wxlworkhard <491538285@qq.com>
2015-04-01 17:32:11 +0800

    MOD: donut chart 修改

--- app/scripts/common/directives/donut_chart.js
+++ app/scripts/common/directives/donut_chart.js
@@ -10,8 +10,10 @@
         return {
             restrict: 'A',
             replace: true,
-            template: '<div class="donut_chart" style="width: 150px;height: 150px;margin: 0 auto;">' +
-                       '<img src="ngsrc/ajax-loading.gif" style="width:100px;"></div>',
+            template: 
+                '<div class="donut_chart">' +
+                    '<img class="donut_chart__loading_img" src="ngsrc/ajax-loading.gif">' + 
+                '</div>',
             link: function (scope, element, attrs) {
                 scope.$on('paintDonutChart', function (event, data) {
                     var id = element.attr('id');
@@ -24,31 +26,40 @@
                         data = parseFloat(data.deficit_rate);
                     }
                     var dataRest = 100 - data;
+                    
                     var options = {
                         chart: {
-                            type: 'pie'
+                            type: 'pie',
+                            width: 150,
+                            height: 150
                         },
+
                         title: {
                             text: ''
                         },
+
                         exporting: {
                             enabled: false
                         },
+
                         credits: {
                             enabled: false
                         },
+
                         plotOptions: {
                             pie: {
                                 shadow: false,
                                 center: ['50%', '50%']
                             }
                         },
+
                         tooltip: {
                             formatter: function () {
                                 return '<span style="font-weight:700;">' + 
                                         this.y + '%</span>'
                             }
                         },
+
                         series: [{
                             data: [{
                                 y: data,
@@ -67,7 +78,6 @@
                     };
 
                     element.highcharts(options);
-
                 });
             }
         };
