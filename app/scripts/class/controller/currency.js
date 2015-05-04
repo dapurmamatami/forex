1bdf96960265a017874cb8554e9726373c9a67b8
fuwenbin <187210346@qq.com>
2015-05-04 11:09:45 +0800

    bug fix

--- app/scripts/class/controller/currency.js
+++ app/scripts/class/controller/currency.js
@@ -1,30 +1,29 @@
 (function(){
     'use strict';
-
     angular
       .module('tigerwitPersonalApp')
       .controller('ClassCurrencyController',ClassCurrencyController);
-
-    ClassCurrencyController.$inject = ['$scope','$http','$state']
-
+    ClassCurrencyController.$inject = ['$scope','$http','$state'];
     function ClassCurrencyController($scope,$http,$state){
 
-          $scope.getCurrency= getCurrency;
-          $scope.linkDetail = linkDetail;
-          function linkDetail(className){
-              window.symbol_detail_single = $scope.mData;
-              $state.go('class.detail',{className:className});
-          }
-          function getCurrency(){
+        $scope.getCurrency= getCurrency;
+        $scope.linkDetail = linkDetail;
 
-              $http.get('/symbol_list',{
-                  params:{
-                      tab:1
-                  }
-                }).then(function(data){
-                  $scope.mData = data;
-              })
-          }
-          getCurrency();
+        function linkDetail(className){
+            window.symbol_detail_single = $scope.mData;
+            $state.go('class.detail',{className:className});
         }
+        function getCurrency(){
+
+            $http.get('/symbol_list',{
+                params:{
+                    tab:1
+                }
+              }).then(function(data){
+                $scope.mData = data;
+            })
+        }
+        getCurrency();
+    }
+    console.info("test4");
 })();
