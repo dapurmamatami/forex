bc002b1b8de6fc8454ca1885662f719edd4ef8c3
wangxiaolong <491538285@qq.com>
2015-05-05 14:37:19 +0800

    MOD: 数据统计默认显示一个月数据

--- app/scripts/account/controllers/register_demo.js
+++ app/scripts/account/controllers/register_demo.js
@@ -11,7 +11,7 @@
 
     function AccountRegisterDemoController($window, $rootScope, $scope, $state, 
             $q, $timeout, account, validator) {
-        $scope.step = 2;
+        $scope.step = 1;
         $scope.account = {
             //username: ,
             //phone: ,
