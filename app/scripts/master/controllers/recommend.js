26ca65ccc4994c69555e1bc3b307a19a52e247e0
fuwenbin <187210346@qq.com>
2015-05-07 13:58:20 +0800

    fix bugs

--- app/scripts/master/controllers/recommend.js
+++ app/scripts/master/controllers/recommend.js
@@ -4,20 +4,20 @@
 	angular.module('tigerwitPersonalApp')
 	.controller("MasterRecommendController",MasterRecommendController);
 
-	MasterRecommendController.$inject = ['$scope','$http'];
+	MasterRecommendController.$inject = ['$scope','$http','$state'];
 
-	function MasterRecommendController($scope,$http){
+	function MasterRecommendController($scope,$http,$state){
+
+		$scope.skipToSummary = skipToSummary;
 
 		$http.get('/recommend_list',{params:{}}).then(function(data){
 
 			if(data.is_succ){
-				console.info(data);
 				$scope.mData = data.data;
-
-
 			}
 		});
-
-	}
-
+		function skipToSummary(touchId){
+          $state.go('invest.subPage',{userCode:touchId,subPage:'summary'});
+        }
+    }
 })();
